fba2ddfdefe8a575019ffb8fd5a77496238cff72
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-14 14:35:55 +0100

    feat: add user queries

---
+++ src/lib/db/queries/users.ts
@@ -0,0 +1,25 @@
+import { eq } from "drizzle-orm";
+import { db } from "..";
+import { users } from "../schema";
+
+async function createUser(name: string) {
+  const [result] = await db.insert(users).values({ name: name }).returning();
+  return result;
+}
+
+async function getUser(name: string) {
+  return await db.select().from(users).where(eq(users.name, name));
+}
+
+async function getAllUsers() {
+  const result = await db.select().from(users);
+  const addedUsers = result.map((user) => user.name);
+  return addedUsers;
+}
+
+async function deleteAllUsers() {
+  const result = await db.delete(users);
+  return result[1];
+}
+
+export { createUser, getUser, getAllUsers, deleteAllUsers };
