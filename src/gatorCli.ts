6d23c9a2c9679ddc9e1587b4ee9658907589dee2
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-14 14:37:15 +0100

    feat: add register, reset and users handlers

--- src/gatorCli.ts
+++ src/gatorCli.ts
@@ -1,7 +1,12 @@
-import { setUser } from "./config";
+import { readConfig, setUser } from "./config";
+import {
+  createUser,
+  getAllUsers,
+  deleteAllUsers,
+} from "./lib/db/queries/users";
 
 // command registry and its helper functions
-type CommandHandler = (cmdName: string, ...args: string[]) => void;
+type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
 type CommandsRegisty = Record<string, CommandHandler>;
 
 function registerCommand(
@@ -12,28 +17,71 @@ function registerCommand(
   registry[cmdName] = handler;
 }
 
-function runCommad(
+async function runCommand(
   registry: CommandsRegisty,
   cmdName: string,
   ...args: string[]
 ) {
   const handler = registry[cmdName];
-  handler(cmdName, ...args);
+  await handler(cmdName, ...args);
 }
 
 // command handlers
-function handlerLogin(cmdName: string, ...args: string[]): void {
+async function handlerLogin(cmdName: string, ...args: string[]): Promise<void> {
   if (args.length === 0) {
     throw new Error("username is required");
   }
 
   const userName = args[0];
+  const users = await getAllUsers();
+  if (!users.find((user) => user === userName)) {
+    throw new Error("user cannot be found");
+  }
+
   setUser(userName);
-  console.log(`${userName} has been set as the username.`);
+  console.log(`${userName} logged in and is now the current user.`);
+}
+
+async function handlerRegister(cmdName: string, ...args: string[]) {
+  if (args.length === 0) {
+    throw new Error("username must be specified");
+  }
+
+  try {
+    const user = await createUser(args[0]);
+    setUser(user.name);
+    console.log(`${user.name} account has been created.
+
+${user.name} details:
+-------------
+ID: ${user.id}
+Name: ${user.name}
+Created At: ${user.createdAt.toISOString()}
+Updated At: ${user.updatedAt.toISOString()}
+`);
+  } catch (error) {
+    throw new Error("user has already registered");
+  }
+}
+
+async function handlerReset(cmdName: string, ...args: string[]) {
+  await deleteAllUsers();
+  console.log("db has been reset");
+}
+
+async function handlerUsers(cmdName: string, ...args: string[]) {
+  const allUsers = await getAllUsers();
+  const currentUser = readConfig().currentUserName;
+  const others = allUsers.filter((user) => user !== currentUser);
+  console.log(`* ${currentUser} (current)`);
+  others.map((user) => console.log(`* ${user}`));
 }
 
 // command registers
 const registry: CommandsRegisty = {};
 registerCommand(registry, "login", handlerLogin);
+registerCommand(registry, "register", handlerRegister);
+registerCommand(registry, "reset", handlerReset);
+registerCommand(registry, "users", handlerUsers);
 
-export { runCommad, registry};
+export { runCommand, registry };
