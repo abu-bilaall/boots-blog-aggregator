e9a7cfcb890bfc5ddad812797a2bcac0e001716d
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-16 12:49:19 +0100

    feat: add addfeed and agg handlers

--- src/gatorCli.ts
+++ src/gatorCli.ts
@@ -1,9 +1,13 @@
+import { createFeed, deleteAllFeeds } from "lib/db/queries/feeds";
 import { readConfig, setUser } from "./config";
 import {
   createUser,
   getAllUsers,
   deleteAllUsers,
+  getUser,
 } from "./lib/db/queries/users";
+import { fetchFeed } from "./rss";
+import { Feed, User } from "lib/db/schema";
 
 // command registry and its helper functions
 type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
@@ -24,6 +28,34 @@ async function runCommand(
 ) {
   const handler = registry[cmdName];
   await handler(cmdName, ...args);
+}
+
+function printFeed(feed: Feed, user: User) {
+  console.log(`
+Feed added successfully!
+
+Feed Details:
+-------------
+\tName: ${feed.name}
+\tURL: ${feed.url}
+\tID: ${feed.id}
+\tCreated At: ${feed.createdAt.toISOString()}
+\tUpdated At: ${feed.updatedAt.toISOString()}
+
+User:
+-----
+\tName: ${user.name}
+\tID: ${user.id}
+`);
+}
+
+function isHttpUrl(url: string): boolean {
+  try {
+    const validUrl = new URL(url);
+    return validUrl.protocol === "http:" || validUrl.protocol === "https:";
+  } catch (error) {
+    return false;
+  }
 }
 
 // command handlers
@@ -66,6 +98,7 @@ Updated At: ${user.updatedAt.toISOString()}
 
 async function handlerReset(cmdName: string, ...args: string[]) {
   await deleteAllUsers();
+  await deleteAllFeeds;
   console.log("db has been reset");
 }
 
@@ -77,11 +110,69 @@ async function handlerUsers(cmdName: string, ...args: string[]) {
   others.map((user) => console.log(`* ${user}`));
 }
 
+async function handlerAgg(cmdName: string, ...args: string[]) {
+  if (args.length === 0) {
+    throw new Error("feed must be specified");
+  }
+
+  try {
+    const feed = await fetchFeed(args[0]);
+    // const feed = await fetchFeed("https://www.wagslane.dev/index.xml");
+
+    const itemsFormatted = feed.channel.item
+      .map((item, index) => {
+        return `\tItem ${index + 1}:
+\t\tTitle: ${item.title}
+\t\tDescription: ${item.description}
+\t\tLink: ${item.link}`;
+      })
+      .join("\n\n");
+
+    console.log(`${feed.channel.title} has been fetched successfully.
+
+${feed.channel.title} details:
+-------------
+Title: ${feed.channel.title}
+Description: ${feed.channel.description}
+Link: ${feed.channel.link}
+Item:
+${itemsFormatted}
+`);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function handlerAddFeed(cmdName: string, ...args: string[]) {
+  if (args.length < 2) {
+    throw new Error("Name and URL of the feed must be specified.")
+  }
+
+  const feedName = args[0];
+  const feedUrl = args[1];
+  
+  if (!isHttpUrl(feedUrl)) {
+    throw new Error("Invalid URL. See example: 'addfeed \"feed name\" \"https://gator.cli/\"'");
+  }
+
+  try {
+    const user = readConfig().currentUserName;
+    const userDetails = await getUser(user);
+    const userId = userDetails[0].id;
+    const feed = await createFeed(feedName, feedUrl, userId);
+    printFeed(feed, userDetails[0]);
+  } catch (error) {
+    throw new Error(`${args[0]} has already been added`);
+  }
+}
+
 // command registers
 const registry: CommandsRegisty = {};
 registerCommand(registry, "login", handlerLogin);
 registerCommand(registry, "register", handlerRegister);
 registerCommand(registry, "reset", handlerReset);
 registerCommand(registry, "users", handlerUsers);
+registerCommand(registry, "agg", handlerAgg);
+registerCommand(registry, "addfeed", handlerAddFeed);
 
 export { runCommand, registry };
