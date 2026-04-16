ba3ab38cd021e4d2be867f257fe1f6752aa49a39
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-16 12:46:29 +0100

    feat: add feeds table to schema

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,6 +1,6 @@
 import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
+const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom().notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at")
@@ -9,3 +9,22 @@ export const users = pgTable("users", {
     .$onUpdate(() => new Date()),
   name: text("name").notNull().unique(),
 });
+
+const feeds = pgTable("feeds", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  name: text("name").notNull(),
+  url: text("url").notNull().unique(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+});
+
+type Feed = typeof feeds.$inferSelect;
+type User = typeof users.$inferSelect;
+
+export { users, feeds, User, Feed };
