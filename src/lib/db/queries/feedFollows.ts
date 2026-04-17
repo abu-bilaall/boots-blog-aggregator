f45881562d9feab1325b85756c46f6709aa4fb36
Abdulrofihi Musa <abdulrofihimusa@gmail.com>
2026-04-17 14:46:55 +0100

    feat: add query functions for feedFollows

---
+++ src/lib/db/queries/feedFollows.ts
@@ -0,0 +1,40 @@
+import { db } from "..";
+import { feeds, users, feedFollows } from "../schema";
+import { eq } from "drizzle-orm";
+
+async function createFeedFollow(userId: string, feedId: string) {
+  const [newFeedFollow] = await db
+    .insert(feedFollows)
+    .values({ userId, feedId })
+    .returning();
+
+  const result = await db
+    .select({
+      id: feedFollows.id,
+      createdAt: feedFollows.createdAt,
+      updatedAt: feedFollows.updateAt,
+      feedName: feeds.name,
+      userName: users.name,
+    })
+    .from(feedFollows)
+    .innerJoin(feeds, eq(feeds.id, newFeedFollow.id))
+    .innerJoin(users, eq(users.id, newFeedFollow.userId));
+  
+  return result;
+}
+
+async function getFeedFollowsForUser(userId: string) {
+  const result = await db
+    .select({
+      feedName: feeds.name,
+      userName: users.name,
+    })
+    .from(feedFollows)
+    .innerJoin(feeds, eq(feeds.id, feedFollows.feedId))
+    .innerJoin(users, eq(users.id, feedFollows.userId))
+    .where(eq(feedFollows.userId, userId));
+
+  return result.map((info) => info.feedName);
+}
+
+export { createFeedFollow, getFeedFollowsForUser };
